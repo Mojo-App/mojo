f0cd1a468d961922e7feb538c043a94ad61883c7
Craig Moss <craig@io.co.za>
2022-07-09 19:06:44 +0200

    Hardhat config to Polygon and some Contract basics

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,79 +15,13 @@ module.exports = {
       blockGasLimit: 1350000000,
       gas: 1300000000,
     },
+    // Alchemy cuurently using Polygon Mumbai Testnet
     alchemy: {
       url: process.env.ALCHEMY_API_URL,
       key: process.env.ALCHEMY_API_KEY,
       accounts: [process.env.PRIVATE_KEY],
-    },
-    testnet: {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-      chainId: 97,
-      gasPrice: 20000000000,
-      accounts: {
-        mnemonic: 'test test test test test test test test test test test junk',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: '',
-      },
-    },
-    mainnet: {
-      url: 'https://bsc-dataseed.binance.org/',
-      chainId: 56,
-      gasPrice: 20000000000,
-      accounts: {
-        mnemonic: 'test test test test test test test test test test test junk',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: '',
-      },
-    },
-    matic: {
-      url: 'https://rpc-mainnet.matic.network',
-      accounts: {
-        mnemonic: 'test test test test test test test test test test test junk',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: '',
-      },
-      blockGasLimit: 20000000,
-      gasPrice: 1000000000,
-    },
-    mumbai: {
-      url: 'https://rpc-mumbai.matic.today',
-      accounts: {
-        mnemonic: 'test test test test test test test test test test test junk',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: '',
-      },
-      blockGasLimit: 20000000,
-      gasPrice: 1000000000,
-    },
-    kovan: {
-      url: 'https://kovan.infura.io/',
-      accounts: {
-        mnemonic: 'test test test test test test test test test test test junk',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: '',
-      },
-      gasPrice: 5000000000,
-    },
-    rinkeby: {
-      url: 'https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde',
-      accounts: {
-        mnemonic: 'test test test test test test test test test test test junk',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: '',
-      },
+      blockGasLimit: 1350000000,
+      gas: 1300000000,
     },
   },
   etherscan: {
